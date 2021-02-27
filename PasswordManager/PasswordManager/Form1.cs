1cfd304f60da271cf7b07117e0624c7d8c5177d2
Theis-Mathiassen <78611080+Theis-Mathiassen@users.noreply.github.com>
2021-02-27 15:51:22 +0100

    Clear passwords correct.

--- PasswordManager/PasswordManager/Form1.cs
+++ PasswordManager/PasswordManager/Form1.cs
@@ -33,10 +33,9 @@ namespace PasswordManager
 
         private void loadToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Program.passwordBook.passwordData.Clear();
-            dataGridView1.Rows.Clear();
-
             
+            dataGridView1.Rows.Clear();
+            Program.passwordBook.clearPasswords();
             Program.passwordBook.LoadPasswordsFromCSV(Program.passwordListPath);
             insertDataToGridView(Program.passwordBook.passwordData, dataGridView1);
             updatePasswordsView();
